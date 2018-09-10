81e2b37c7331040df7e96455b631462361c299a2
Ryan Elandt <ryan.elandt@gmail.com>
2018-09-10 15:55:27 -0400

    set maximum line length to 120 characters

--- src/principal_value.jl
+++ src/principal_value.jl
@@ -3,9 +3,14 @@ mod_minus_pi_to_pi(a::T) where {T} = mod2pi(a + pi) - pi
 """
     principal_value(R::Rotation{3})
 
-**Background:** All non `RotMatrix` rotation types can represent the same `RotMatrix` in two or more ways. Sometimes a particular set of numbers is better conditioned (e.g. `SPQuat`) or obeys a particular convention (e.g. `AngleAxis` has non-negative rotation). In order to preserve differentiability it is necessary to allow rotation representations to travel slightly away from the nominal domain; this is critical for applications such as optimization or dynamics. 
+**Background:** All non `RotMatrix` rotation types can represent the same `RotMatrix` in two or more ways. Sometimes a
+particular set of numbers is better conditioned (e.g. `SPQuat`) or obeys a particular convention (e.g. `AngleAxis` has
+non-negative rotation). In order to preserve differentiability it is necessary to allow rotation representations to
+travel slightly away from the nominal domain; this is critical for applications such as optimization or dynamics. 
 
-This function takes a rotation type (e.g. `Quat`, `RotXY`) and outputs a new rotation of the same type that corresponds to the same `RotMatrix`, but that obeys certain conventions or is better conditioned. The outputs of the function have the following properties:
+This function takes a rotation type (e.g. `Quat`, `RotXY`) and outputs a new rotation of the same type that corresponds
+to the same `RotMatrix`, but that obeys certain conventions or is better conditioned. The outputs of the function have
+the following properties:
 
 - all angles are between between `-pi` to `pi` (except for `AngleAxis` which is between `0` and `pi`). 
 - all `Quat` have non-negative real part

7d17131d3b59ff279f46712950191a7caaf13d80
Twan Koolen <koolen.twan@gmail.com>
2018-09-12 11:44:59 -0400

    Minor performance improvements.

--- src/principal_value.jl
+++ src/principal_value.jl
@@ -6,13 +6,13 @@ mod_minus_pi_to_pi(a::T) where {T} = mod2pi(a + pi) - pi
 **Background:** All non `RotMatrix` rotation types can represent the same `RotMatrix` in two or more ways. Sometimes a
 particular set of numbers is better conditioned (e.g. `SPQuat`) or obeys a particular convention (e.g. `AngleAxis` has
 non-negative rotation). In order to preserve differentiability it is necessary to allow rotation representations to
-travel slightly away from the nominal domain; this is critical for applications such as optimization or dynamics. 
+travel slightly away from the nominal domain; this is critical for applications such as optimization or dynamics.
 
 This function takes a rotation type (e.g. `Quat`, `RotXY`) and outputs a new rotation of the same type that corresponds
 to the same `RotMatrix`, but that obeys certain conventions or is better conditioned. The outputs of the function have
 the following properties:
 
-- all angles are between between `-pi` to `pi` (except for `AngleAxis` which is between `0` and `pi`). 
+- all angles are between between `-pi` to `pi` (except for `AngleAxis` which is between `0` and `pi`).
 - all `Quat` have non-negative real part
 - the components of all `SPQuat` have a norm that is at most 1.
 - the `RodriguesVec` rotation is at most `pi`
@@ -25,9 +25,9 @@ principal_value(spq::SPQuat{T}) where {T} = convert(SPQuat, principal_value(conv
 function principal_value(aa::AngleAxis{T}) where {T}
     theta = mod_minus_pi_to_pi(aa.theta)
     if theta < zero(T)
-        return AngleAxis(-theta, -aa.axis_x, -aa.axis_y, -aa.axis_z)
+        return AngleAxis(-theta, -aa.axis_x, -aa.axis_y, -aa.axis_z, false)
     else
-        return AngleAxis( theta,  aa.axis_x,  aa.axis_y,  aa.axis_z)
+        return AngleAxis( theta,  aa.axis_x,  aa.axis_y,  aa.axis_z, false)
     end
 end
 
